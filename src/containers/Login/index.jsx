6bfa18f72e30db1bb8606268bc1b66a270e06d3b
kaio candido <kaio.candf@gmail.com>
2024-09-11 20:39:04 -0300

    adicionado tela de login, adicionado notificações, conectado a api com axios e criado o arquivo de rotas

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -4,13 +4,14 @@ import { Button } from "../../components/Button";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
+import { api } from "../../services/api";
+import { toast } from 'react-toastify';
 
 export function Login() {
     const schema = yup.object({
         email: yup.string().email("Digite um E-mail Válido").required("Digite um email"),
         password: yup.string().min(6, "A senha deve ter no minimo 6 caracteres").required("Digite Uma Senha"),
     }).required()
-
     const {
         register,
         handleSubmit,
@@ -18,7 +19,19 @@ export function Login() {
     } = useForm({
         resolver: yupResolver(schema),
     })
-    const onSubmit = (data) => console.log(data)
+    const onSubmit = async (data) => {
+        const response = await toast.promise(
+            api.post("/session", {
+                email: data.email,
+                password: data.password,
+            }),
+            {
+                pending: "Verificando Seus Dados",
+                success: "Seja Bem-Vindo(a)",
+                error: "Email ou Senha incorretos",
+            }
+        )
+    }
 
     return (
         <Container>
@@ -26,7 +39,7 @@ export function Login() {
                 <img src={Logo} alt="Logo-DevBurguer" />
             </LeftContainer>
             <RightContainer>
-                <Title>Olá, seja bem vindo ao <span>Dev Burguer!</span><br />
+                <Title>Olá, seja bem vindo ao <span>New Burguer!</span><br />
                     Acesse com seu <span>Login e senha.</span></Title>
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <InputContainer>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Login } from './containers/Login'
 import globalStyles from './styles/globalStyles'
+import { ToastContainer } from 'react-toastify'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <RouterProvider router={router} />
     <globalStyles />
+    <ToastContainer autoClose={2000} theme='colored' />
   </StrictMode>,
 )
