56035827276999696d094492363b0b5772ddf17f
kaio candido <kaio.candf@gmail.com>
2024-09-17 16:40:14 -0300

    carrousel add

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react"
 import { api } from "../../services/api"
 import { useState } from "react"
+import { Carousel } from 'react-multi-carousel';
+import { Container, Title, ContainerItems } from "./styles"
+import "react-multi-carousel/lib/styles.css"
 
-export function CategoriesCarousel(params) {
+export function CategoriesCarousel() {
     const [categories, setCategories] = useState([])
     useEffect(() => {
         async function loadCategories() {
@@ -15,9 +18,45 @@ export function CategoriesCarousel(params) {
         loadCategories()
     }, [])
 
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 4,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1280 },
+            items: 4,
+        },
+        tablet: {
+            breakpoint: { max: 1280, min: 690 },
+            items: 3,
+        },
+        mobile: {
+            breakpoint: { max: 690, min: 0 },
+            items: 2,
+        },
+    }
+
     return (
-        <div>
-            <h1>Ok</h1>
-        </div>
+        <Container>
+            <Title>Categorias</Title>
+            <Carousel
+                responsive={responsive}
+                infinite={true}
+                partialVisible={false}
+                itemClass="carousel-item"
+            >
+                {categories.map(category => (
+                    <ContainerItems key={category.id} imageUrl={category.url}>
+                        <p>{category.name}</p>
+                    </ContainerItems>
+                ))}
+
+            </Carousel>
+
+
+
+        </Container>
     )
 }
+
